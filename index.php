eea6d6047fc0fc4905000d5306523dd9f4dabd03
Nejc Pirečnik <nejcpirecnik03@gmail.com>
2021-09-27 13:44:48 +0200

    Upload

--- index.php
+++ index.php
@@ -18,10 +18,34 @@
 
     <!-- Style css -->
     <link rel="stylesheet" href="assets/css/style.css">
+    <style>
+        .no-js #loader { display: none;  }
+        .js #loader { display: block; position: absolute; left: 100px; top: 0; }
+        .se-pre-con {
+            position: fixed;
+            left: 0px;
+            top: 0px;
+            width: 100%;
+            height: 100%;
+            z-index: 9999;
+            background: url(assets/img/Preloader.gif) center no-repeat #000000;
+        }
+    </style>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js"></script>
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.2/modernizr.js"></script>
+    <script>
 
+        $(window).load(function() {
+            // Animate loader off screen
+            $(".se-pre-con").fadeOut("slow");;
+        });
+    </script>
 </head>
 
 <body>
+<!-- Paste this code after body tag -->
+<div class="se-pre-con"></div>
+<!-- Ends -->
     <div class="main">
         <?php include 'header.php' ?>
         <!-- ***** Hero Area Start ***** -->
